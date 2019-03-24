523a18c43414bd2351903fdbb831346177ed7851
RaymondLoranger <Raymond.Loranger@bell.net>
2019-03-23 20:22:08 -0400

    0.1.0

---
+++ config/persist_app.exs
@@ -0,0 +1,4 @@
+use Mix.Config
+
+config :islands_island,
+  island_types: [:atoll, :dot, :l_shape, :s_shape, :square]
---
+++ lib/islands/island/offsets.ex
@@ -0,0 +1,21 @@
+defmodule Islands.Island.Offsets do
+  @moduledoc """
+  Returns a list of offsets for each island type.
+  """
+
+  alias Islands.Island
+
+  @type t :: [{0..2, 0..2}]
+
+  @doc """
+  Returns a list of offsets for a given island type.
+  """
+  @spec new(Island.type()) :: t | {:error, atom}
+  def new(island_type)
+  def new(:atoll), do: [{0, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}]
+  def new(:dot), do: [{0, 0}]
+  def new(:l_shape), do: [{0, 0}, {1, 0}, {2, 0}, {2, 1}]
+  def new(:s_shape), do: [{0, 1}, {0, 2}, {1, 0}, {1, 1}]
+  def new(:square), do: [{0, 0}, {0, 1}, {1, 0}, {1, 1}]
+  def new(_unknown), do: {:error, :invalid_island_type}
+end
